9ec308417ea7439978adaf2d25984d217fe88507
Pitttike <gabor.wittmann.uzlet@gmail.com>
2024-11-26 01:14:59 +0100

    fet: input validation

--- src/components/Felvetel.tsx
+++ src/components/Felvetel.tsx
@@ -6,14 +6,12 @@ export default function Felvetel() {
     const [receiver, setReceiver] = useState<string>("");
     const [subject, setSubject] = useState<string>("");
     const [content, setContent] = useState<string>("");
+    const [error, setError] = useState<string[]>([]);
 
-    const [error, setError] = useState<string | null>(null);
-    const [success, setSuccess] = useState<boolean>(false);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        setError(null);
-
+        setError([]);
 
         const newEmail = {
             sender: sender,
@@ -33,16 +31,16 @@ export default function Felvetel() {
 
             if (!response.ok) {
                 const errorData = await response.json();
-                setError(errorData.error);
-                throw new Error(`Hiba történt: ${response.status}`);
+                setError(errorData);
+
             }
-            setSuccess(true);
+
             setSender('');
             setReceiver('');
             setSubject('');
             setContent('');
         } catch (err: any) {
-            setError(err.message);
+            setError([err.message]);
         }
     };
 
@@ -50,21 +48,30 @@ export default function Felvetel() {
         <h1>E-mail felvétele</h1>
 
         <label htmlFor="sender">Feladó:</label>
-        <input type="text" name="sender" value={sender} onChange={e => setSender(e.target.value)} />
+        <input type="text" name="sender" value={sender} onChange={e => setSender(e.target.value)} required/>
 
         <label htmlFor="receiver">Címzett:</label>
-        <input type="text" name="receiver" value={receiver} onChange={e => setReceiver(e.target.value)} />
+        <input type="text" name="receiver" value={receiver} onChange={e => setReceiver(e.target.value)} required/>
 
         <label htmlFor="subject">Tárgy:</label>
-        <input type="text" name="subject" value={subject} onChange={e => setSubject(e.target.value)} />
+        <input type="text" name="subject" value={subject} onChange={e => setSubject(e.target.value)} required/>
 
         <label htmlFor="content">Üzenet:</label>
-        <input type="text" name="content" value={content} onChange={e => setContent(e.target.value)} />
+        <textarea 
+            name="content" 
+            value={content} 
+            onChange={e => setContent(e.target.value)}
+            className="content"
+            rows={4}
+            cols={50}
+            required
+        />
 
-        <button type="submit">E-mail felvétele</button>
+        {error && error.map((e: string) => <p style={{ color: 'red' }}>{e}</p>)}
+        {error.length === 0 && <p style={{ color: 'green' }}>Üzenet sikeresen elküldve</p>}
+        
+        <button type="submit">Küldés</button>
 
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {success && <p style={{ color: 'green' }}>Sikeresen megtörtént a tablet felvétele.</p>}
 
     </form>
 }
